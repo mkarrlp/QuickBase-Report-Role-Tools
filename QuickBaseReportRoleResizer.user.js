5a390041531de6d4b7c5c1be2dc0c3471b04d2ba
Michael Karr <45976981+mkarrlp@users.noreply.github.com>
2019-04-17 16:37:47 -0700

    Update QuickBaseReportRoleResizer.user.js

--- QuickBaseReportRoleResizer.user.js
+++ QuickBaseReportRoleResizer.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         QuickBase Report Role Resizer
-// @namespace    http://tampermonkey.net/
+// @namespace    QuickBaseReportRoleResizer
 // @version      0.1
 // @description  Resizes the Role list in reports to make it easier to navigate and find Roles
-// @author       You
+// @author       mkarrlp
 // @match        ://*.quickbase.com/db/*?a=viewbuild&qid*
 // @grant        none
 // @updateURL    ://github.com/mkarrlp/QuickBaseReportRoleViewResizer/raw/master/QuickBaseReportRoleResizer.user.js
